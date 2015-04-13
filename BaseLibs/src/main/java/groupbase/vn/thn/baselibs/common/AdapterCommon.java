1231e0a2175b95bc2ced8de531964ad80ae9db28
TruongHieuNghia <truonghieunghia.code@gmail.com>
2015-04-13 22:09:07 +0700

    update

--- BaseLibs/src/main/java/groupbase/vn/thn/baselibs/common/AdapterCommon.java
+++ BaseLibs/src/main/java/groupbase/vn/thn/baselibs/common/AdapterCommon.java
@@ -1,6 +1,5 @@
 package groupbase.vn.thn.baselibs.common;
 
-<<<<<<< HEAD
 import android.app.FragmentManager;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -13,17 +12,15 @@ import java.util.List;
 import java.util.Objects;
 
 import groupbase.vn.thn.baselibs.listener.AdapterBaseListener;
-=======
+
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
->>>>>>> 026cf072780b9b484a886813429329da9b93fc58
 
 /**
  * Created by nghiath on 4/3/15.
  */
-<<<<<<< HEAD
 public class AdapterCommon<T> extends ArrayAdapter<T> {
 
     protected FragmentManager mFragmentManager;
@@ -31,7 +28,8 @@ public class AdapterCommon<T> extends ArrayAdapter<T> {
     private AdapterBaseListener mAdapterBaseListener;
     private int mLayoutId;
     private Objects mHolderView;
-    private List<T>lst;
+    private List<T> lst;
+
     public AdapterCommon(Context context, int resource, List<T> objects) {
         super(context, resource, objects);
         mLayoutId = resource;
@@ -47,7 +45,7 @@ public class AdapterCommon<T> extends ArrayAdapter<T> {
         lst = objects;
     }
 
-    public <T,H> void setAdapterBaseListener(AdapterBaseListener<T,H> adapterBaseListener) {
+    public <T, H> void setAdapterBaseListener(AdapterBaseListener<T, H> adapterBaseListener) {
         this.mAdapterBaseListener = adapterBaseListener;
 
     }
@@ -62,29 +60,22 @@ public class AdapterCommon<T> extends ArrayAdapter<T> {
         return position;
     }
 
-    public void setTagHolder(Objects holder){
+    public void setTagHolder(Objects holder) {
         this.mHolderView = holder;
     }
+
     @Override
-    public  View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
         if (view == null) {
             view = mInflater.inflate(this.mLayoutId, parent, false);
-            mHolderView = (Objects)this.mAdapterBaseListener.setHolderView(view);
+            mHolderView = (Objects) this.mAdapterBaseListener.setHolderView(view);
             view.setTag(mHolderView);
         } else {
             mHolderView = (Objects) view.getTag();
         }
-        mAdapterBaseListener.showData(getItem(position),mHolderView,position);
+        mAdapterBaseListener.showData(getItem(position), mHolderView, position);
         return view;
-=======
-public class AdapterCommon < T > extends ArrayAdapter< T > {
-
-    private int mXmlLayout;
-    public AdapterCommon ( Context context, int xmlLayout, List< T > list ) {
-
-        super( context, xmlLayout, list );
-        mXmlLayout = xmlLayout;
->>>>>>> 026cf072780b9b484a886813429329da9b93fc58
     }
+
 }
