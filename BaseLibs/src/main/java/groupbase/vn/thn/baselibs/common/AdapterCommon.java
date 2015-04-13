e6b37cf0ac370c44d18a4614eeb650d8c4c4ab91
TruongHieuNghia <truonghieunghia.code@gmail.com>
2015-04-13 22:04:51 +0700

    update

--- BaseLibs/src/main/java/groupbase/vn/thn/baselibs/common/AdapterCommon.java
+++ BaseLibs/src/main/java/groupbase/vn/thn/baselibs/common/AdapterCommon.java
@@ -1,8 +1,73 @@
 package groupbase.vn.thn.baselibs.common;
 
+import android.app.FragmentManager;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Objects;
+
+import groupbase.vn.thn.baselibs.listener.AdapterBaseListener;
+
 /**
  * Created by nghiath on 4/3/15.
  */
-public class AdapterCommon {
+public class AdapterCommon<T> extends ArrayAdapter<T> {
 
+    protected FragmentManager mFragmentManager;
+    protected LayoutInflater mInflater;
+    private AdapterBaseListener mAdapterBaseListener;
+    private int mLayoutId;
+    private Objects mHolderView;
+    private List<T>lst;
+    public AdapterCommon(Context context, int resource, List<T> objects) {
+        super(context, resource, objects);
+        mLayoutId = resource;
+        mInflater = LayoutInflater.from(context);
+        lst = objects;
+    }
+
+    public AdapterCommon(Context context, int resource, List<T> objects, FragmentManager fm) {
+        super(context, resource, objects);
+        mFragmentManager = fm;
+        mLayoutId = resource;
+        mInflater = LayoutInflater.from(context);
+        lst = objects;
+    }
+
+    public <T,H> void setAdapterBaseListener(AdapterBaseListener<T,H> adapterBaseListener) {
+        this.mAdapterBaseListener = adapterBaseListener;
+
+    }
+
+    @Override
+    public int getCount() {
+        return lst.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public void setTagHolder(Objects holder){
+        this.mHolderView = holder;
+    }
+    @Override
+    public  View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = mInflater.inflate(this.mLayoutId, parent, false);
+            mHolderView = (Objects)this.mAdapterBaseListener.setHolderView(view);
+            view.setTag(mHolderView);
+        } else {
+            mHolderView = (Objects) view.getTag();
+        }
+        mAdapterBaseListener.showData(getItem(position),mHolderView,position);
+        return view;
+    }
 }
--- BaseLibs/src/main/java/groupbase/vn/thn/baselibs/listener/AdapterBaseListener.java
+++ BaseLibs/src/main/java/groupbase/vn/thn/baselibs/listener/AdapterBaseListener.java
@@ -1,7 +1,11 @@
 package groupbase.vn.thn.baselibs.listener;
 
+import android.view.View;
+
 /**
  * Created by TruongHieuNghia on 4/13/15.
  */
-public interface AdapterBaseListener {
+public interface AdapterBaseListener<T,H> {
+    public H setHolderView(View view);
+    public void showData(T data,H holderView,int position);
 }
